5cb2df036e3bd8340bc5cd3cb5945d7c7257e0d7
joao rosis <jrosisoliveira123@gmail.com>
2023-12-04 14:24:19 +0000

    added second and third highest probabilities

--- api/doodle_api.py
+++ api/doodle_api.py
@@ -16,7 +16,7 @@ modelDraw= tf.keras.models.load_model('/modelBaseLine.keras')
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 
@@ -40,13 +40,26 @@ async def predict(image: Drawing):
     image_np = np.array(image_image)
     image_tensor = tf.convert_to_tensor(image_np)
     image_tensor = tf.expand_dims(image_tensor, axis=0)
+    image_tensor = tf.image.resize(image_tensor, [64, 64])
     predictionDraw = modelDraw.predict(image_tensor)
     label_list = QuickDrawData().drawing_names
-    drawClass = label_list[np.argmax(predictionDraw)]
-    ProbabilityDraw = float(np.max(predictionDraw))
+
+    first_draw = label_list[np.argmax(predictionDraw)]
+    first_prob= float(np.max(predictionDraw))
+
+
+    second_draw = label_list[np.argsort(np.argmax(predictionDraw, axis = 0))[-2]]
+    second_prob = float(np.partition(predictionDraw.flatten(), -2)[-2])
+
+
+    third_draw = label_list[np.argsort(np.argmax(predictionDraw, axis = 0))[-3]]
+    third_prob = float(np.partition(predictionDraw.flatten(), -3)[-3])
+
+
     result_dict = {
-                "Draw":drawClass,
-                "Probability" :ProbabilityDraw
+                first_draw:first_prob ,
+                second_draw: second_prob,
+                third_draw:third_prob
                    }
     return   result_dict
 #####
